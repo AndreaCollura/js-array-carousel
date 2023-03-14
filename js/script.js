5679ce4a3a77a292ac6ec775b996e4848ac29c0e
AndreaCollura <andrea.collur@gmail.com>
2023-03-14 18:07:55 +0100

    added style + border on active thumb feature

--- js/script.js
+++ js/script.js
@@ -7,6 +7,42 @@
 //  `
 
 
+{/* <div class="sliderthumb">
+            <div class="row d-flex flex-column align-items-center">
+                <div class="col-6 ">
+                    <div class="thumbox">
+                        <img src="./img/01.webp" alt="">
+                    </div>
+                </div>
+                <div class="col-6 ">
+                    <div class="thumbox">
+                        <img src="./img/02.webp" alt="">
+                    </div>
+                </div>
+                <div class="col-12 ">
+                    <div class="thumbox">
+                        <img src="./img/03.webp" alt="">
+                    </div>
+                </div>
+                <div class="col-12 ">
+                    <div class="thumbox">
+                        <img src="./img/04.webp" alt="">
+                    </div>
+                </div>
+                <div class="col-6 ">
+                    <div class="thumbox">
+                        <img src="./img/05.webp" alt="">
+                    </div>
+                </div>
+            </div>
+
+        </div> */}
+
+
+
+
+
+        
 const imgs = [
     'img/01.webp',
     'img/02.webp',
@@ -14,6 +50,57 @@ const imgs = [
     'img/04.webp',
     'img/05.webp'
 ]
+
+
+
+ const slideThumb = document.getElementById('thumbnails');
+ let thumBox = '';
+ for (let i = 0 ; i < imgs.length ; i++){
+     thumBox = `
+     <div class="sliderthumb">
+         <div class="row d-flex flex-column align-items-center">
+             <div class="col-6 ">
+                 <div class="thumbox">
+                     <img src="./img/01.webp" alt="">
+                 </div>
+             </div>
+             <div class="col-6 ">
+                 <div class="thumbox">
+                     <img src="./img/02.webp" alt="">
+                 </div>
+             </div>
+             <div class="col-12 ">
+                 <div class="thumbox">
+                     <img src="./img/03.webp" alt="">
+                 </div>
+             </div>
+             <div class="col-12 ">
+                 <div class="thumbox">
+                     <img src="./img/04.webp" alt="">
+                 </div>
+             </div>
+             <div class="col-6 ">
+                 <div class="thumbox">
+                     <img src="./img/05.webp" alt="">
+             </div>
+             <div>
+                <button class="up"><i class="fa fa-chevron-circle-up" ></i></i></button>
+                <button class="down"><i class="fa fa-chevron-circle-down" ></i></button>
+            </div>
+        </div>
+     </div>
+  
+  
+     `
+ }
+
+ slideThumb.innerHTML = thumBox;
+
+
+
+
+
+
 
 
 const slider = document.querySelector('.slider');
@@ -44,6 +131,8 @@ next.addEventListener('click', nextImg);
 
 function nextImg (){
     document.querySelectorAll('.slide')[imgsCounter].classList.remove('active');
+    document.querySelectorAll('.thumbox')[imgsCounter].classList.remove('light-border');
+    
     if (imgsCounter === imgs.length -1){
         imgsCounter = 0;
     } else {
@@ -51,6 +140,7 @@ function nextImg (){
     }
   
     document.querySelectorAll('.slide')[imgsCounter].classList.add('active');
+    document.querySelectorAll('.thumbox')[imgsCounter].classList.add('light-border');
 
 }
 
@@ -59,6 +149,7 @@ prev.addEventListener('click', prevImg);
 
 function prevImg (){
     document.querySelectorAll('.slide')[imgsCounter].classList.remove('active');
+    document.querySelectorAll('.thumbox')[imgsCounter].classList.remove('light-border');
     if (imgsCounter === 0){
         imgsCounter = imgs.length -1;
         
@@ -67,6 +158,7 @@ function prevImg (){
     }
   
     document.querySelectorAll('.slide')[imgsCounter].classList.add('active');
+    document.querySelectorAll('.thumbox')[imgsCounter].classList.add('light-border');
 
 }
 
